f001e4d021c9347de11d21be646b741a3a78e10e
Lyon.Nee <1057476454@qq.com>
2019-07-09 23:56:08 +0800

    first push

---
+++ Certify/Instance/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Certify.Instance
+{
+    public class Transaction : ISerializable
+    {
+        public string hash { get; set; }
+
+        public string timeStamp { get; set; }
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+           
+        }
+    }
+}
---
+++ Certify/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Certify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
